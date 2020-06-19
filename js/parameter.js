f8e3d28ec360b1d5b565e06020b7ade56233e7c5
jun <silentken21@gmail.com>
2020-06-19 18:49:36 +0900

    first commit

---
+++ js/parameter.js
@@ -0,0 +1,202 @@
+var parameter = [
+   {
+      "seq": 1,
+      "row_val": "aa",
+      "row_val_2": "11",
+      "co_val": "as",
+      "col_val_1": "qq",
+      "revenue": 114,
+      "pu": 68,
+      "cu": 113
+   },
+   {
+      "seq": 2,
+      "row_val": "aa",
+      "row_val_2": "22",
+      "co_val": "sd",
+      "col_val_1": "ee",
+      "revenue": 49,
+      "pu": 66,
+      "cu": 59
+   },
+   {
+      "seq": 3,
+      "row_val": "aa",
+      "row_val_2": "33",
+      "co_val": "df",
+      "col_val_1": "rr",
+      "revenue": 90,
+      "pu": 73,
+      "cu": 99
+   },
+   {
+      "seq": 4,
+      "row_val": "bb",
+      "row_val_2": "11",
+      "co_val": "as",
+      "col_val_1": "ww",
+      "revenue": 66,
+      "pu": 52,
+      "cu": 71
+   },
+   {
+      "seq": 5,
+      "row_val": "bb",
+      "row_val_2": "22",
+      "co_val": "df",
+      "col_val_1": "qq",
+      "revenue": 113,
+      "pu": 71,
+      "cu": 104
+   },
+   {
+      "seq": 6,
+      "row_val": "bb",
+      "row_val_2": "33",
+      "co_val": "sd",
+      "col_val_1": "rr",
+      "revenue": 68,
+      "pu": 73,
+      "cu": 65
+   },
+   {
+      "seq": 7,
+      "row_val": "cc",
+      "row_val_2": "33",
+      "co_val": "df",
+      "col_val_1": "ww",
+      "revenue": 60,
+      "pu": 81,
+      "cu": 55
+   },
+   {
+      "seq": 8,
+      "row_val": "cc",
+      "row_val_2": "22",
+      "co_val": "sd",
+      "col_val_1": "qq",
+      "revenue": 47,
+      "pu": 80,
+      "cu": 56
+   },
+   {
+      "seq": 9,
+      "row_val": "dd",
+      "row_val_2": "11",
+      "co_val": "as",
+      "col_val_1": "ee",
+      "revenue": 36,
+      "pu": 82,
+      "cu": 40
+   },
+   {
+      "seq": 10,
+      "row_val": "dd",
+      "row_val_2": "22",
+      "co_val": "sd",
+      "col_val_1": "rr",
+      "revenue": 56,
+      "pu": 88,
+      "cu": 51
+   },
+   {
+      "seq": 11,
+      "row_val": "aa",
+      "row_val_2": "11",
+      "co_val": "as",
+      "col_val_1": "qq",
+      "revenue": 38,
+      "pu": 80,
+      "cu": 39
+   },
+   {
+      "seq": 12,
+      "row_val": "aa",
+      "row_val_2": "22",
+      "co_val": "sd",
+      "col_val_1": "ee",
+      "revenue": 56,
+      "pu": 86,
+      "cu": 45
+   },
+   {
+      "seq": 13,
+      "row_val": "aa",
+      "row_val_2": "33",
+      "co_val": "df",
+      "col_val_1": "rr",
+      "revenue": 59,
+      "pu": 82,
+      "cu": 56
+   },
+   {
+      "seq": 14,
+      "row_val": "bb",
+      "row_val_2": "11",
+      "co_val": "as",
+      "col_val_1": "ww",
+      "revenue": 38,
+      "pu": 92,
+      "cu": 49
+   },
+   {
+      "seq": 15,
+      "row_val": "bb",
+      "row_val_2": "22",
+      "co_val": "df",
+      "col_val_1": "qq",
+      "revenue": 51,
+      "pu": 95,
+      "cu": 43
+   },
+   {
+      "seq": 16,
+      "row_val": "bb",
+      "row_val_2": "33",
+      "co_val": "sd",
+      "col_val_1": "rr",
+      "revenue": 52,
+      "pu": 88,
+      "cu": 53
+   },
+   {
+      "seq": 17,
+      "row_val": "cc",
+      "row_val_2": "33",
+      "co_val": "df",
+      "col_val_1": "ww",
+      "revenue": 68,
+      "pu": 104,
+      "cu": 76
+   },
+   {
+      "seq": 18,
+      "row_val": "cc",
+      "row_val_2": "22",
+      "co_val": "sd",
+      "col_val_1": "qq",
+      "revenue": 59,
+      "pu": 94,
+      "cu": 56
+   },
+   {
+      "seq": 19,
+      "row_val": "dd",
+      "row_val_2": "11",
+      "co_val": "as",
+      "col_val_1": "ee",
+      "revenue": 61,
+      "pu": 93,
+      "cu": 61
+   },
+   {
+      "seq": 20,
+      "row_val": "dd",
+      "row_val_2": "22",
+      "co_val": "sd",
+      "col_val_1": "rr",
+      "revenue": 45,
+      "pu": 78,
+      "cu": 44
+   }
+]
